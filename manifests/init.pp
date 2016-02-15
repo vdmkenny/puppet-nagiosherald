b19573b2b08c42100b9d6190d0c3af8f37f700a9
vdmkenny <vdmkenny@inuits.eu>
2016-02-15 15:32:50 +0100

    instead of git now download package

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,14 +1,9 @@
 class nagiosherald{
 
-    package{'git':
+
+    package{'nagios-herald':
         ensure => present,
     }
-
-    exec{ 'git clone':
-    command => 'git clone https://github.com/etsy/nagios-herald.git /usr/local/nagios-herald/',
-    path    => '/usr/local/bin/:/bin/',
-    # path    => [ '/usr/local/bin/', '/bin/' ],  # alternative syntax
-  	}
 
     file{'/etc/icinga/objects/commands/notify-by-herald.cfg':
 	owner  => root,
