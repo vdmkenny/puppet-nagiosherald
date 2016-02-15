81afa9ca579df6d3f2836416363a5a70313047e5
vdmkenny <vdmkenny@inuits.eu>
2016-02-15 19:34:55 +0100

    templated notify-by-herald.cfg

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,17 +1,17 @@
 class nagiosherald(
     
     
-    $graphiteserver = 'graphite2.internal.inuits.eu',
-    $logstashserver = 'https://kibana.inuits.eu:9200',
-    $logstashfrontend = 'https://kibana.inuits.eu/index.html#/dashboard/file/logstash.json?from=%{from}&to=%{to}&query=%{query}',
+    $graphiteserver = 'graphite.example.com',
+    $logstashserver = 'http://logstash.example.com:9200',
+    $logstashfrontend = 'http://kibana.example.com/index.html#/dashboard/file/logstash.json?from=%{from}&to=%{to}&query=%{query}',
     $resultfieldtruncate = '200',
     $numresults = '10',
     $logfile = '/tmp/nagios-herald.log',
     $formatterdir = '/usr/local/nagios-herald-formatters',
     $icinga = true,
     
-    $nagios_cgi = 'https://icinga.inuits.eu/cgi-bin/cmd.cgi',
-    $replyto = 'noreply@inuits.eu',
+    $nagios_cgi = 'https://icinga.example.com/cgi-bin/cmd.cgi',
+    $replyto = 'noreply@example.com',
 
     )
     
@@ -24,8 +24,9 @@ class nagiosherald(
 
     file{'/etc/icinga/objects/commands/notify-by-herald.cfg':
 	owner  => root,
-        source => 'puppet:///modules/nagiosherald/notify-by-herald.cfg',
-	}
+        #source => 'puppet:///modules/nagiosherald/notify-by-herald.cfg',
+	content => template('nagiosherald/notify-by-herald.cfg.erb'),
+        }
 
     file{'/usr/local/nagios-herald/etc/config.yml':
         owner  => root,
