18eb0d124c476dc03a2f4d331d7f48e64ab75a5e
vdmkenny <vdmkenny@inuits.eu>
2016-02-23 11:05:47 +0100

    made module to take config parameters

---
+++ manifests/config.pp
@@ -0,0 +1,25 @@
+define nagiosherald::config(
+    $graphiteserver = 'graphite.example.com',
+    $logstashserver = 'http://logstash.example.com:9200',
+    $logstashfrontend = 'http://kibana.example.com/index.html#/dashboard/file/logstash.json?from=%{from}&to=%{to}&query=%{query}',
+    $resultfieldtruncate = '200',
+    $numresults = '10',
+    $logfile = '/tmp/nagios-herald.log',
+    $formatterdir = '/usr/local/nagios-herald-formatters',
+    $icinga = true,
+    $nagios_cgi = 'https://icinga.example.com/cgi-bin/cmd.cgi',
+    $replyto = 'noreply@example.com',
+   ){
+    
+    file{'/etc/icinga/objects/commands/notify-by-herald.cfg':
+	owner  => root,
+        #source => 'puppet:///modules/nagiosherald/notify-by-herald.cfg',
+	content => template('nagiosherald/notify-by-herald.cfg.erb'),
+        }
+
+    file{'/usr/local/nagios-herald/etc/config.yml':
+        owner  => root,
+        #source => 'puppet:///modules/nagiosherald/config.yml',
+        content => template('nagiosherald/config.yml.erb'),
+    }
+}
